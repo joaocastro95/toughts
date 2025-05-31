fdbcd389cf36a8536a30c7b282058469a34d3628
Joao Victor Oliveira de Castro <joaov.castro95@gmail.com>
2025-05-30 22:36:52 -0300

    Iniciando projeto

---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('toughts', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectado com sucesso!')
+
+} catch (error) {
+    console.log('Não foi possível conectar: ', error)
+}
+
+module.exports = sequelize
